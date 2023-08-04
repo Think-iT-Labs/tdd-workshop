4c135c84a40267b228cd0e33603e44f5acde5674
Mantey Caleb <manteycaleb@gmail.com>
2023-08-04 15:11:05 +0000

    feat: added e2e test cases for password validation endpoint

--- endtoend.test.ts
+++ endtoend.test.ts
@@ -1,19 +1,46 @@
 import chai from "chai";
 import { expect } from "chai";
 import chaiHttp from "chai-http";
-import { app } from "./http-server"
+import { app } from "./http-server";
 
 chai.use(chaiHttp);
 
+const mocked_returns = {
+  successfull_response: {
+    valid: true,
+    messages: [],
+  },
+  error_response: {
+    valid: false,
+    messages: ["Password length has to be 8 characters"],
+  },
+};
+
 describe("password validation endpoint", () => {
   it("should respond with 200", () => {
-    chai.request(app)
-      .post('/password/validation')
-      .send({ "password": "1234578_123" })
+    chai
+      .request(app)
+      .post("/password/validation")
+      .send({ password: "1234578_123" })
       .end((error, response) => {
-        console.log('is passing here')
+        console.log("is passing here");
         expect(response.ok).to.be.true;
         expect(response.status).to.be.eq(200);
+        expect(response.body).to.be.deep.equal(
+          mocked_returns.successfull_response
+        );
+      });
+  });
+
+  it("should respond with 200", () => {
+    chai
+      .request(app)
+      .post("/password/validation")
+      .send({ password: "12547" })
+      .end((error, response) => {
+        expect(response.ok).to.be.true;
+        expect(response.statusCode).to.be.equal(200);
+        expect(response.body.valid).to.be.deep.equal(mocked_returns.error_response.valid);
       });
   });
 });
--- http-server.ts
+++ http-server.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import { passwordValidator } from "./passwordvalidation";
 
 const port = 3000;
 export const app = express();
+app.use(express.json())
 
 app.post('/password/validation', (req, res: any) => {
-  res.send();
+
+  const response = passwordValidator(req.body.password);
+
+  res.send({
+    valid: response.isValid,
+    messages: response.errors
+  });
 });
