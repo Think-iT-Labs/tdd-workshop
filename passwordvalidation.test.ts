b81ed94e6e365cd6c92301f1a534c87c8b835826
Sohaib Sherif <triples17t@gmail.com>
2023-09-01 16:46:40 +0200

    Introduce PasswordValidator Class

--- passwordvalidation.test.ts
+++ passwordvalidation.test.ts
@@ -1,50 +1,60 @@
 import { expect } from "chai";
-import { passwordValidator } from "./passwordvalidation";
+import { PasswordValidator, passwordValidator } from "./passwordvalidation";
 
 describe("Password validation tests", () => {
   it("it should be at least 8 characters", () => {
-
-    const result =passwordValidator("1234567_")
-    expect(result.errors).to.be.empty
-    expect(result.isValid).to.be.true
-
+    const result = passwordValidator("1234567_");
+    expect(result.errors).to.be.empty;
+    expect(result.isValid).to.be.true;
   });
 
   it("it should return an error when the password is less than 8 characters ", () => {
-    const result = passwordValidator('1234567');
+    const result = passwordValidator("1234567");
     expect(result.isValid).to.be.false;
-    expect(result.errors).to.contain('Password must be at least 8 characters')
- 
-  })
+    expect(result.errors).to.contain("Password must be at least 8 characters");
+  });
 
-  it("it should contain at least 2 numbers ",()=> { 
-
-    const result = passwordValidator("fdkjkhd1")
-    expect(result.isValid).to.be.false
-    expect(result.errors).to.contain("The password must contain at least 2 numbers")
-  })
+  it("it should contain at least 2 numbers ", () => {
+    const result = passwordValidator("fdkjkhd1");
+    expect(result.isValid).to.be.false;
+    expect(result.errors).to.contain(
+      "The password must contain at least 2 numbers"
+    );
+  });
 
   it("should return a list of errors if validation rules are not met", () => {
-    const result = passwordValidator("some2")
+    const result = passwordValidator("some2");
 
-    expect(result.isValid).to.be.false
-    expect(result.errors).to.contain("Password must be at least 8 characters")
-    expect(result.errors).to.contain("The password must contain at least 2 numbers")
-  })
+    expect(result.isValid).to.be.false;
+    expect(result.errors).to.contain("Password must be at least 8 characters");
+    expect(result.errors).to.contain(
+      "The password must contain at least 2 numbers"
+    );
+  });
 
   it("should failed when the password doesn't have any number", () => {
-    const result = passwordValidator("some")
+    const result = passwordValidator("some");
 
-    expect(result.isValid).to.be.false
-    expect(result.errors).to.contain("The password must contain at least 2 numbers")
-  })
+    expect(result.isValid).to.be.false;
+    expect(result.errors).to.contain(
+      "The password must contain at least 2 numbers"
+    );
+  });
 
-  it("should contain at least one special character", ()=> {
-    const result =passwordValidator("12345678")
-    expect(result.isValid).to.be.false
-    expect(result.errors).to.contain("The password must contain at least one special character")
+  it("should contain at least one special character", () => {
+    const result = passwordValidator("12345678");
+    expect(result.isValid).to.be.false;
+    expect(result.errors).to.contain(
+      "The password must contain at least one special character"
+    );
+  });
 
-  })
- 
-  
+  it("should fail when the password is a common illegal password", () => {
+    const passwordValidatObject = new PasswordValidator();
+    passwordValidatObject.setIllegalPasswords(["illegal", "illegal2"]);
+
+    const result = passwordValidatObject.validate("illegal");
+    expect(result.isValid).to.be.false;
+    expect(result.errors).to.contain("The password is illegal as too common");
+  });
 });
--- passwordvalidation.ts
+++ passwordvalidation.ts
@@ -1,23 +1,38 @@
 export const passwordValidator = (password: string) => {
   const numberRegex = /\d/g;
-  const specialCharRegex = /[!@#\$%\^\&*\)\(+=._-]/g
+  const specialCharRegex = /[!@#\$%\^\&*\)\(+=._-]/g;
   const numbers = password.match(numberRegex)?.length || 0;
   const errors = [];
+  const illegalPasswords = ["illegal"];
 
-  if (password.length < 8){
+  if (password.length < 8) {
     errors.push("Password must be at least 8 characters");
   }
 
-  if (numbers < 2){
+  if (numbers < 2) {
     errors.push("The password must contain at least 2 numbers");
   }
-  
-  if(!specialCharRegex.test(password)){
+
+  if (!specialCharRegex.test(password)) {
     errors.push("The password must contain at least one special character");
   }
-  
+
   return {
     isValid: errors.length == 0,
     errors,
   };
 };
+
+export class PasswordValidator {
+  illegal = ["i"];
+  constructor() {}
+
+  validate(password: string) {
+    return {
+      isValid: false,
+      errors: ["The password is illegal as too common"],
+    };
+  }
+
+  setIllegalPasswords(dict: Array<string>) {}
+}
