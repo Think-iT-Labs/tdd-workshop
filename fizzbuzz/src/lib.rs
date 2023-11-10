25b8e7458f2882db7433cd5e55b28354de5df491
Mantey Caleb <manteycaleb@gmail.com>
2023-11-10 15:46:46 +0000

    added strings for mutliples of 3

--- fizzbuzz/src/lib.rs
+++ fizzbuzz/src/lib.rs
@@ -1,16 +1,22 @@
-fn fizzbuzz() -> Vec<usize> {
+fn fizz() -> Vec<String> {
     let mut numbers = vec![1; 100];
     for num in 0..99 {
         numbers[num] += num
     }
-    return numbers
+
+    return numbers.iter().map(|n| if n % 3 == 0 {String::from("fizz")} else { n.to_string()}).collect();
 }
 
 #[test]
 fn should_return_100_numbers() {
-    assert_eq!(fizzbuzz().len(), 100);
+    assert_eq!(fizz().len(), 100);
 }
 #[test]
 fn should_return_43_at_index_42() {
-    assert_eq!(fizzbuzz()[42] ,43)
+    assert_eq!(fizz()[42] ,"43")
+}
+
+#[test]
+fn should_return_fizz_if_is_multiple_of_3() {
+    assert_eq!(fizz()[2], "fizz")
 }
