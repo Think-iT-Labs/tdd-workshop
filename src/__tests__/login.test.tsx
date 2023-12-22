583005e762965a165aa5c74d28541f7d70af1bf2
WalaMotamed <walamotamed@think-it.io>
2023-12-22 16:39:41 +0100

    adds initial tests for login

---
+++ src/__tests__/login.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { Sample } from "../components/index";
+import "@testing-library/jest-dom";
+
+describe("Login", () => {
+  it("renders a login form", () => {
+    render(<Sample />);
+
+    const usernameInput = screen.getByTestId("username-input");
+    const passwordInput = screen.getByTestId("password-input");
+    const loginButton = screen.getByRole("button",  { name:"Login"})
+
+    expect(usernameInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it("should be disabled when empty inputs ", () => {
+    render(<Sample />);
+
+    const usernameInput = screen.getByTestId("username-input");
+    const passwordInput = screen.getByTestId("password-input");
+    const loginButton = screen.getByRole("button",  { name:"Login"})
+
+    expect(loginButton).toHaveAttribute('disabled');
+  });
+});
