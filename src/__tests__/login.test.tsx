e73d0adb86e82be18a34107a7f6be902d2192834
Selim Ben Slimene <62063931+selimbens@users.noreply.github.com>
2023-12-22 17:06:36 +0100

    test if button is enabled on input

--- src/__tests__/login.test.tsx
+++ src/__tests__/login.test.tsx
@@ -1,18 +1,28 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Sample } from "../components/index";
 import "@testing-library/jest-dom";
 
 describe("Login", () => {
-  it("renders a login form", () => {
+  // it("renders a login form", () => {
+  //   render(<Sample />);
+
+  //   const usernameInput = screen.getByTestId("username-input");
+  //   const passwordInput = screen.getByTestId("password-input");
+  //   const loginButton = screen.getByRole("button",  { name:"Login"})
+
+  //   expect(usernameInput).toBeInTheDocument();
+  //   expect(passwordInput).toBeInTheDocument();
+  //   expect(loginButton).toBeInTheDocument();
+  // });
+
+  it("should be disabled when empty inputs ", () => {
     render(<Sample />);
 
     const usernameInput = screen.getByTestId("username-input");
     const passwordInput = screen.getByTestId("password-input");
-    const loginButton = screen.getByRole("button",  { name:"Login"})
+    const loginButton = screen.getByRole("button", { name: "Login" });
 
-    expect(usernameInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
-    expect(loginButton).toBeInTheDocument();
+    expect(loginButton).toHaveAttribute("disabled");
   });
 
   it("should be disabled when empty inputs ", () => {
@@ -20,8 +30,11 @@ describe("Login", () => {
 
     const usernameInput = screen.getByTestId("username-input");
     const passwordInput = screen.getByTestId("password-input");
-    const loginButton = screen.getByRole("button",  { name:"Login"})
+    const loginButton = screen.getByRole("button", { name: "Login" });
 
-    expect(loginButton).toHaveAttribute('disabled');
+    fireEvent.change(usernameInput, { target: { value: "username" } });
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
+
+    expect(loginButton).not.toHaveAttribute("disabled");
   });
 });
--- src/components/sample.tsx
+++ src/components/sample.tsx
@@ -1,3 +1,29 @@
+"use client"
+import { useState } from "react";
+
 export const Sample = () => {
-  return <h2>Welcome to the app</h2>;
+  const [disabled, setDisabled] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <form className="text-black">
+      <input
+        data-testid="username-input"
+        type="text"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button className="text-white" name="Login" disabled={!username && !password}>
+        Login
+      </button>
+    </form>
+  );
 };
