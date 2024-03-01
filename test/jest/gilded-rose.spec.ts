32ecf904f20bb7c1b72151f5306c10565b4c534a
ndr_brt <andrea.bertagnolli@gmail.com>
2024-03-01 15:17:23 +0100

    cover first 2 requirements

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,22 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('should lower sellIn and quality by one', () => {
+    const gildedRose = new GildedRose([new Item('foo', 5, 5)]);
+
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+
+    expect(items[0].name).toBe('foo');
+    expect(items[0].sellIn).toBe(4);
+    expect(items[0].quality).toBe(4);
+  });
+
+  it('should lower quality by two when sell by date has been passed', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 5)]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(3);
   });
 });
